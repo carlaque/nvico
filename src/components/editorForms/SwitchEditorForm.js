6d7995da46d1d77b23b2e96408225be21d9ea49b
carlaque <andrade.carla12@gmail.com>
2022-04-22 20:18:23 -0300

    fix labels on switch popup

--- src/components/editorForms/SwitchEditorForm.js
+++ src/components/editorForms/SwitchEditorForm.js
@@ -1,18 +1,18 @@
 import React, { useCallback } from 'react';
 
-export const SwitchEditorForm = ({ device, setDevice, handleSubmit, handleInputChange}) => {
+export const SwitchEditorForm = ({ device, setDevice, handleSubmit, handleInputChange }) => {
 
     return (
         <div className="switchEdit">
             <form onSubmit={handleSubmit} >
-                <label> Quantidade de Portas </label>
+                <label> Display Name </label>
                 <input
                     type="text"
                     value={device.name}
                     onChange={handleInputChange}
                     name="name"
                 />
-                <label> Display Name </label>
+                <label> Quantidade de Portas </label>
                 <input
                     type="number"
                     value={device.maxConnections}
